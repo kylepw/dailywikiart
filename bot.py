9580cfe7257ff89fadf23815a20d7b1bb35ba975
Kyle <kylepw@gmail.com>
2019-05-08 10:27:01 +0900

    Fix key var mispelling

--- bot.py
+++ bot.py
@@ -4,18 +4,17 @@ import os
 
 load_dotenv()
 
-API_KEY = os.getenv('API_KEY')
-API_SECRET = os.getenv('API_SECRET') 
-ACCESS_KEY = os.getenv('ACCESS_KEY')
+CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+CONSUMER_SECRET = os.getenv('CONSUMER_SECRET')
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 ACCESS_SECRET = os.getenv('ACCESS_SECRET')
 
 def main():
-    auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
     api = tweepy.API(auth)
 
-    user = api.me()
-    print(user.name)
+    api.update_status('hey')
 
 if __name__ == '__main__':
     main()
